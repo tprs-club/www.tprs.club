696ce79843a37465fa782190ecb082d903f0af5f
Qiwen Zhao <qiwen.zhao@gmail.com>
2019-02-03 18:18:50 -0800

    Add changing connection string for docker process

--- script/build.sh
+++ script/build.sh
@@ -4,7 +4,11 @@ SCRIPT_DIR=$(dirname $0)
 
 pushd $SCRIPT_DIR/../web
 rm -fr dist
+cp src/connection.js src/__saved.js
+sed "s,'.*','http://node:4000',g" src/connection.js
 npm run build
+cp src/__saved.js src/connection.js
+rm -f src/__saved.js
 docker build -t qiwenzhao/tbrs.web:latest .
 popd
 
